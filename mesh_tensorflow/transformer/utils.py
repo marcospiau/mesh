ba6000e93b6f5d7a3ff3008d9974395f59580f10
marcospiau <marcospiau123@gmail.com>
2023-11-06 22:38:20 +0000

    Fix vocab.encode calling, add checks

--- mesh_tensorflow/transformer/utils.py
+++ mesh_tensorflow/transformer/utils.py
@@ -1515,11 +1515,16 @@ def decode_from_file(estimator,
     dataset = dataset.prefetch(tf.data.experimental.AUTOTUNE)
     return dataset
 
-  # getting False and True token ids for monot5
-  false_token_id,  true_token_id = map(vocabulary.encode,
+  # Getting False and True token ids for monot5.
+  # This is probably extra careful, but it is better to be safe than sorry.
+  assert (
+    vocab := inputs_vocabulary(vocabulary)) == targets_vocabulary(vocabulary)
+  false_token_id,  true_token_id = map(vocab.encode,
                                        monot5_false_true_tokens)
   assert len(false_token_id) == len(true_token_id) == 1
   false_token_id, true_token_id = false_token_id[0], true_token_id[0]
+  # just for tests [852, 4295] # Não/Sim
+  assert false_token_id, true_token_id == [852, 4295]
   # log tokens and token_ids
   tf.logging.info("mono5_false_true_tokens: %s", monot5_false_true_tokens)
   tf.logging.info("false_token_id: %s", false_token_id)
